f6f276a76a84628559205ad0f01ecad36fe76a64
Stephen <swrobbin@gmail.com>
2021-09-26 13:59:40 -0400

    commit tree 3f1a98e7f0715fd4a189713c785c3baf09695615

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,8 +6,8 @@ function Form() {
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input onChange={handleFirstNameChange} type="text" value={firstName} />
+      <input onChange={handleLastNameChange} type="text" value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
